a4b35a3661a38f55434e8b74978e0a9aa9430829
haybek <haybekbatuhan@gmail.com>
2023-07-11 16:00:01 +0300

    Form Login Screen tasarimi

--- NutriGuide.UI/LoginScreen.Designer.cs
+++ NutriGuide.UI/LoginScreen.Designer.cs
@@ -28,12 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnLogin = new Button();
+            lblUserName = new Label();
+            label1 = new Label();
+            lblYazi = new Label();
+            lblSignUp = new Label();
+            panel1 = new Panel();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            txtPassword = new TextBox();
+            txtUsername = new TextBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Bahnschrift SemiCondensed", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLogin.Location = new Point(63, 255);
+            btnLogin.Margin = new Padding(4);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(386, 49);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "LOGIN";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblUserName.Location = new Point(63, 89);
+            lblUserName.Margin = new Padding(4, 0, 4, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(88, 18);
+            lblUserName.TabIndex = 3;
+            lblUserName.Text = "Username";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Courier New", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(63, 174);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 18);
+            label1.TabIndex = 4;
+            label1.Text = "Password";
+            // 
+            // lblYazi
+            // 
+            lblYazi.AutoSize = true;
+            lblYazi.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblYazi.Location = new Point(123, 324);
+            lblYazi.Margin = new Padding(4, 0, 4, 0);
+            lblYazi.Name = "lblYazi";
+            lblYazi.Size = new Size(146, 16);
+            lblYazi.TabIndex = 5;
+            lblYazi.Text = "Don't have an account?";
+            // 
+            // lblSignUp
+            // 
+            lblSignUp.AutoSize = true;
+            lblSignUp.Font = new Font("Courier New", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblSignUp.Location = new Point(277, 322);
+            lblSignUp.Margin = new Padding(4, 0, 4, 0);
+            lblSignUp.Name = "lblSignUp";
+            lblSignUp.Size = new Size(88, 18);
+            lblSignUp.TabIndex = 6;
+            lblSignUp.Text = "Sign Up!";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(txtUsername);
+            panel1.Controls.Add(lblSignUp);
+            panel1.Controls.Add(lblYazi);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(lblUserName);
+            panel1.Location = new Point(657, 50);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(471, 394);
+            panel1.TabIndex = 7;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox5.Location = new Point(415, 203);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(32, 31);
+            pictureBox5.TabIndex = 11;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox4.Location = new Point(415, 117);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(32, 32);
+            pictureBox4.TabIndex = 10;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox2.Location = new Point(184, 19);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 50);
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Courier New", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            txtPassword.Location = new Point(63, 203);
+            txtPassword.Margin = new Padding(4);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(384, 31);
+            txtPassword.TabIndex = 8;
+            // 
+            // txtUsername
+            // 
+            txtUsername.Font = new Font("Courier New", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            txtUsername.Location = new Point(63, 118);
+            txtUsername.Margin = new Padding(4);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(384, 31);
+            txtUsername.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.Location = new Point(1116, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(32, 31);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox3);
+            panel2.Location = new Point(68, 95);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(530, 359);
+            panel2.TabIndex = 9;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.Control;
+            pictureBox3.Image = Properties.Resources.NGLogo;
+            pictureBox3.Location = new Point(18, 19);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(491, 330);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1160, 567);
+            ControlBox = false;
+            Controls.Add(panel2);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+        private Button btnLogin;
+        private Label lblUserName;
+        private Label label1;
+        private Label lblYazi;
+        private Label lblSignUp;
+        private Panel panel1;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox2;
+        private Panel panel2;
+        private PictureBox pictureBox3;
     }
 }
