8df714b2e9999fea69e119502f94d8c6241e2fdd
sinantsy <sinantasyapar@gmail.com>
2023-07-12 14:28:04 +0300

    Ara Form Design

--- NutriGuide.UI/MainMenu.cs
+++ NutriGuide.UI/MainMenu.cs
@@ -13,62 +13,64 @@ namespace NutriGuide.UI
     public partial class MainMenu : Form
     {
         bool panelGizlimi = false;
-        public MainMenu(string a,string b)
+        Form activeForm;
+        public MainMenu(string a, string b)
         {
             InitializeComponent();
             label3.Text = a.ToString();
             label4.Text = b.ToString();
-
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
-
             if (panelGizlimi == false)
             {
-
                 panel2.Hide();
                 for (int i = 258; i >= 70; i--)
                 {
-
                     panel3.Width = i;
                     button5.Width = i + 3;
-
-
                 }
                 panelGizlimi = true;
-
-
             }
             else if (panel3.Width == 70)
             {
                 panelGizlimi = false;
                 for (int i = 70; i <= 258; i++)
                 {
-
                     panel3.Width = i;
                     button5.Width = i;
-
-
-
                 }
                 button5.Width = button5.Width + 3;
-
                 panel2.Show();
-
-
-                //panel3.Width = 257;
-                //label1.Width = 257;
-
             }
             label2.Text = panel3.Width.ToString();
+        }
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.pnlChildForm.Controls.Add(childForm);
+            this.pnlChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            OpenChildForm(new Forms.AnaMenu());
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
