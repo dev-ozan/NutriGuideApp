77f76d183c5583fc2d7ab96ff7849be8f28620f1
haybek <haybekbatuhan@gmail.com>
2023-07-13 10:44:08 +0300

    ss

--- NutriGuide.UI/LoginScreen.cs
+++ NutriGuide.UI/LoginScreen.cs
@@ -1,4 +1,6 @@
+using Microsoft.Data.SqlClient;
 using NutriGuide.Entity.Data;
+using NutriGuide.Entity.Enums;
 using System.Text;
 
 namespace NutriGuide.UI
@@ -64,9 +66,57 @@ namespace NutriGuide.UI
             
             if (btnLogin.Text == "LOGIN" && txtPassword.Text != "" && txtUsername.Text != "")
             {
-                Kullanici kullanici = new Kullanici();  
-                kullanici.KullaniciAdi = txtUsername.Text;
-                kullanici.KullaniciPassword = txtPassword.Text;
+                string connectionString = "Data Source=your_server;Initial Catalog=your_database;User ID=your_username;Password=your_password;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Kullanýcý verilerini ekleme
+                    Kullanici kullanici = new Kullanici
+                    {
+                        KullaniciAdi = "john_doe",
+                        KullaniciPassword = "123456",
+                        Ad = "John",
+                        Soyad = "Doe",
+                        Cinsiyet = "Erkek",
+                        DogumTarihi = new DateTime(1990, 1, 1),
+                        DietBaslamaTarihi = DateTime.Now,
+                        DietBitisTarihi = DateTime.Now.AddDays(30),
+                        Kilosu = 75,
+                        Boyu = 180,
+                        HedefKilosu = 70,
+                        KullaniciTipi = KullaniciTipi.Kullanici,
+                        Diyetler = "Gluten-free, Vegan"
+                    };
+
+                    string insertQuery = "INSERT INTO Kullanici (KullaniciAdi, KullaniciPassword, Ad, Soyad, Cinsiyet, DogumTarihi, DietBaslamaTarihi, DietBitisTarihi, Kilosu, Boyu, HedefKilosu, KullaniciTipi, Diyetler) " +
+                        "VALUES (@KullaniciAdi, @KullaniciPassword, @Ad, @Soyad, @Cinsiyet, @DogumTarihi, @DietBaslamaTarihi, @DietBitisTarihi, @Kilosu, @Boyu, @HedefKilosu, @KullaniciTipi, @Diyetler);";
+
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                        // Parametreleri ekleme
+                        command.Parameters.AddWithValue("@KullaniciAdi", kullanici.KullaniciAdi);
+                        command.Parameters.AddWithValue("@KullaniciPassword", kullanici.KullaniciPassword);
+                        command.Parameters.AddWithValue("@Ad", kullanici.Ad);
+                        command.Parameters.AddWithValue("@Soyad", kullanici.Soyad);
+                        command.Parameters.AddWithValue("@Cinsiyet", kullanici.Cinsiyet);
+                        command.Parameters.AddWithValue("@DogumTarihi", kullanici.DogumTarihi);
+                        command.Parameters.AddWithValue("@DietBaslamaTarihi", kullanici.DietBaslamaTarihi);
+                        command.Parameters.AddWithValue("@DietBitisTarihi", kullanici.DietBitisTarihi);
+                        command.Parameters.AddWithValue("@Kilosu", kullanici.Kilosu);
+                        command.Parameters.AddWithValue("@Boyu", kullanici.Boyu);
+                        command.Parameters.AddWithValue("@HedefKilosu", kullanici.HedefKilosu);
+                        command.Parameters.AddWithValue("@KullaniciTipi", kullanici.KullaniciTipi.ToString());
+                        command.Parameters.AddWithValue("@Diyetler", kullanici.Diyetler);
+
+                        // Sorguyu çalýþtýrma
+                        command.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine("Kullanýcý verisi baþarýyla eklendi.");
+
+                    connection.Close();
+                }
 
 
                 MainMenu mainMenu = new MainMenu(txtPassword.Text, txtUsername.Text);
