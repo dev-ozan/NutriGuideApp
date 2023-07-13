6514249938f7bec2016976af1b9a98792d23436f
haybek <haybekbatuhan@gmail.com>
2023-07-13 19:54:46 +0300

    besinler duzenlenecek

--- NutriGuide.UI/Forms/Besinler.cs
+++ NutriGuide.UI/Forms/Besinler.cs
@@ -24,12 +24,12 @@ namespace NutriGuide.UI.Forms
         {
             InitializeComponent();
             _kisi = kisi;
-
+            Dongu();
 
             dgvYemekler.DataSource = _db.Foods.ToList();
 
-            cmbDiyetler.Items.Add("Diyet Secin");
-            cmbDiyetler.SelectedIndex = 0;
+            //cmbDiyetler.Items.Add("Diyet Secin");
+            //cmbDiyetler.SelectedIndex = 0;
             if (_kisi.Diyetler != null)
             {
                 foreach (var item in _kisi.Diyetler)
@@ -78,7 +78,25 @@ namespace NutriGuide.UI.Forms
 
         private void cmbDiyetler_SelectedIndexChanged(object sender, EventArgs e)
         {
-            dgvDiyetYemekleri.DataSource = cmbDiyetler.SelectedItem;
+            dgvDiyetYemekleri.DataBindingComplete += (o, _) =>
+            {
+                var dataGridView = o as DataGridView;
+                if (dataGridView != null)
+                {
+                    dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+                    dataGridView.Columns[dataGridView.ColumnCount - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            };
+        }
+
+        public void Dongu()
+        {
+            foreach (var item in _db.Diyetler.Where(x => x.KullaniciId == _kisi.KullaniciId).ToList())
+            {
+                cmbDiyetler.Items.Clear();
+                cmbDiyetler.Items.Add(item.DiyetAdi);
+            }
+
         }
     }
 }
