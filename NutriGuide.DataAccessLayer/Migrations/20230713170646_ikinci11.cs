1c2566dc5f5323838ef2c4a0ec16f9170e0fd7f1
Ibrahim Ozan Genç <ibrahimozan.genc@bilgeadamboost.com>
2023-07-16 10:36:48 +0300

    Güncelleeme

---
+++ NutriGuide.DataAccessLayer/Migrations/20230713170646_ikinci11.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutriGuide.DataAccessLayer.Migrations
+{
+    public partial class ikinci11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
